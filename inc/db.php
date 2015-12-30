0f768aa220a1fb13baec21e870cbd6444d88ccfd
Lionello Lunesu <lio+git@lunesu.com>
2015-12-31 02:34:49 +0800

    Enable SSL

--- inc/db.php
+++ inc/db.php
@@ -1,6 +1,6 @@
 <?php
 	//$link = mysql_connect('localhost')
-        $link = mysql_connect('us-cdbr-azure-west-c.cloudapp.net', 'b86206e3c5f51f', 'a700a05f')
-		or die('mysql_connect error'.mysql_error());
+        $link = mysql_connect('us-cdbr-azure-west-c.cloudapp.net', 'b86206e3c5f51f', 'a700a05f', true, MYSQL_CLIENT_SSL)
+		or die('mysql_connect error '.mysql_error());
         mysql_select_db('mondoxsAJRT4DALX', $link)
-		or die('mysql_select_db error'.mysql_error());
+		or die('mysql_select_db error '.mysql_error());
